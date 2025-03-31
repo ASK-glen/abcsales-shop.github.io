03ef8bd42bf7fa2c43a76e64476edc49e424948f
Jinyoung Jang <jyjang@uengine.org>
2025-03-31 21:18:37 +0900

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/contexts/CartContext";
-import Index from "./pages/Index";
-import Products from "./pages/Products";
-import ProductDetail from "./pages/ProductDetail";
-import Cart from "./pages/Cart";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
 
 const queryClient = new QueryClient();
 
